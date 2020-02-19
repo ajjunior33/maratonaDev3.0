2e2ac6de400299e159b7e2fef6203bffa03c683f
Andre Junior <=>
2020-02-19 10:17:09 -0300

    Dia 02 - [ Finalizando o App ( Concluído).]

--- server.js
+++ server.js
@@ -1,58 +1,61 @@
 const express = require("express");
 const nunjucks = require('nunjucks');
-
+const Pool = require('pg').Pool;
 const server = express();
 
 //Congurando servidor
 server.use(express.static('public'));
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 
+//Configuracao do BANCO
+const db = new Pool({
+    user: 'postgres',
+    password: '0000',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+});
 
 // Configurando a template engine
-nunjucks.configure("./",{
+nunjucks.configure("./", {
     express: server,
     noCache: true
 });
 
-const donors = [
-    {
-        name: "Ana Clara",
-        blood: "AB+"
-    },
-    {
-        name: " Cleiton Souza",
-        blood: "B+"
-    },
-    {
-        name: "Robson Marques",
-        blood: "A+"
-    },
-    {
-        name: " Mayk Brito ",
-        blood: "O+"
-    }
+server.get("/", function (req, res) {
 
+    db.query("SELECT * FROM donors", (err, result) => {
+        if(err) return res.send("Erro de banco de dados.")
 
-];
-
-
-server.get("/", function(req, res){
-    return res.render("index.html", {donors});
+        const donors = result.rows;
+        return res.render("index.html", { donors });
+    });
 });
 
-server.post("/", function(req, res){
+server.post("/", function (req, res) {
     const name = req.body.name;
     const email = req.body.email;
     const blood = req.body.blood;
-    donors.push({
-        name: name, 
-        blood: blood
+    /*
+    //Colocando no Array
+        donors.push({
+            name: name, 
+            blood: blood
+        });
+    */
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obirgatórios.");
+    }
+    const query = `INSERT INTO "donors" ("name", "email", "blood") 
+    VALUES ($1, $2, $3)`;
+    const values = [name, email, blood];
+    db.query(query, values, (err) => {
+        if(err) return res.send("Erro no banco de dados.");
+        return res.redirect("/");
     });
-
-    return res.redirect("/");
 });
 
 
-server.listen(3333, () =>{
+server.listen(3333, () => {
     console.log("start server");
 });
