6abf557bf2db057da333c9705e7681d8f1a4c6c6
unknown <ajjunior33@gmail.com>
2020-02-18 23:14:23 -0300

    Dia 02 - [Finalizando o App ( Não terminei ainda). ]

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+//Congurando servidor
+server.use(express.static('public'));
+server.use(express.urlencoded({extended: true}));
+
+
+// Configurando a template engine
+nunjucks.configure("./",{
+    express: server,
+    noCache: true
+});
+
+const donors = [
+    {
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+    {
+        name: " Cleiton Souza",
+        blood: "B+"
+    },
+    {
+        name: "Robson Marques",
+        blood: "A+"
+    },
+    {
+        name: " Mayk Brito ",
+        blood: "O+"
+    }
+
+
+];
+
+
+server.get("/", function(req, res){
+    return res.render("index.html", {donors});
+});
+
+server.post("/", function(req, res){
+    const name = req.body.name;
+    const email = req.body.email;
+    const blood = req.body.blood;
+    donors.push({
+        name: name, 
+        blood: blood
+    });
+
+    return res.redirect("/");
+});
+
+
+server.listen(3333, () =>{
+    console.log("start server");
+});
